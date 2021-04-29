0ad450b6f0f92635838f55bd260589c41530a490
TahirIsmail <vjtary104@gmail.com>
2021-04-29 00:47:17 -0700

    app js integrated with onauthstatechange open subscription(firebase)

--- src/App.js
+++ src/App.js
@@ -5,19 +5,35 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header-component/header.component.jsx';
+import { auth } from './firebase/firebase.utils';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/signin" component={SignInSignUpPage} />  
-      </Switch>
-    </div>
-  );
+  }
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+    
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
