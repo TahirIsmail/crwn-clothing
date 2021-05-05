a80c74e3c88adf5c4c2dd7a125185eec739ab9b4
TahirIsmail <vjtary104@gmail.com>
2021-05-05 04:03:48 -0700

    fixed typo and signin functionality

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth,signInWithGoogle} from '../../firebase/firebase.utils';
 class SignIn extends React.Component{
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             email: '',
             password: ''
         };
     }
-    handleSubmit = event => {
+    handleSubmit =  async event => {
         event.preventDefault();
-        this.setState({ email: '', password: '' });
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' });
+        }
+        catch (error) {
+            console.log('Error Message',error.message);
+        }
+        
     }
     handleChange = event => {
         const { name, value } = event.target;
@@ -25,16 +33,16 @@ class SignIn extends React.Component{
                 <h1>I Already have an Account!</h1>
                 <span>Sign in with email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} label="email" required />
+                    <FormInput name='email' type='email' value={this.state.email} onChange={this.handleChange} label="email" required />
                     <FormInput
                         name='password'
                         type='password'
                         value={this.state.password}
-                        handleChange={this.handleChange}
+                        onChange={this.handleChange}
                         label='password'
                         required />
                     <div className='buttons'>
-                    <CustomButton type="submit">Sign In</CustomButton>
+                    <CustomButton type="submit" >Sign In</CustomButton>
                     <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
                         {''}
                         Sign in with Google
