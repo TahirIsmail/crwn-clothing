e11f57a72d57408c1b3faa2c50017f85da0957d9
TahirIsmail <vjtary104@gmail.com>
2021-05-10 03:26:08 -0700

    Redirect and User Types are set as constants to avoid typos

--- src/App.js
+++ src/App.js
@@ -1,55 +1,75 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import './App.css';
-import {Route,Switch} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header-component/header.component.jsx';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header-component/header.component';
 import { auth, createUserProfDocument } from './firebase/firebase.utils';
-import { connect } from 'react-redux';
-import {setCurrentUser} from './redux/user/user-actions'
+import { setCurrentUser } from './redux/user/user-actions';
+
 class App extends React.Component {
-  
-  unsubscribeFromAuth = null
+  unsubscribeFromAuth = null;
+
   componentDidMount() {
     const { setCurrentUser } = this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfDocument(userAuth);
+
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
-          },() => (console.log(this.state)))
-          //console.log(this.currentUser);
+            id: snapShot.id,
+            ...snapShot.data()
+          });
         });
       }
-      else {
-        setCurrentUser( userAuth );
-      }
+
+      setCurrentUser(userAuth);
     });
-    
   }
+
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInSignUpPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser : user => dispatch(setCurrentUser(user))
- })
-export default connect(null,mapDispatchToProps)(App);
-//dbname=crwndb-01
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
