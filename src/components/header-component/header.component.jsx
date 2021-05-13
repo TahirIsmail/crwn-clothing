f39f9ee21ba1385be7b5f74b8ccb14bf5fbed56c
TahirIsmail <vjtary104@gmail.com>
2021-05-13 11:57:00 -0700

    cart icon component --no-funtionality

--- src/components/header-component/header.component.jsx
+++ src/components/header-component/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component'
 import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 const Header = ({currentUser}) => (
@@ -25,6 +26,7 @@ const Header = ({currentUser}) => (
                 SIGN IN
                 </Link>
             }
+        <CartIcon/>
         </div>
     </div>
 )
